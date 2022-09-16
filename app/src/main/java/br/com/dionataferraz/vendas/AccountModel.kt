000ed5c100b68940002200b472712b9ddf63d088
Macximus <macx.selbach@gmail.com>
2022-09-15 21:51:06 -0300

    criando tela de cadastro de conta

---
+++ app/src/main/java/br/com/dionataferraz/vendas/AccountModel.kt
@@ -0,0 +1,8 @@
+package br.com.dionataferraz.vendas
+
+data class AccountModel(
+    val accountName: String,
+    val balance: String,
+    val responsible: String,
+    val accountType: String
+)
--- app/src/main/java/br/com/dionataferraz/vendas/HomeActivity.kt
+++ app/src/main/java/br/com/dionataferraz/vendas/HomeActivity.kt
@@ -1,8 +1,11 @@
 package br.com.dionataferraz.vendas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.dionataferraz.vendas.databinding.ActivityHomeBinding
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class HomeActivity : AppCompatActivity() {
 
@@ -10,9 +13,43 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityHomeBinding.inflate(layoutInflater).run {
-            binding = this
-            setContentView(root)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
+        binding.btCreateAccount.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val sharedPreferences = getSharedPreferences(
+            "Account",
+            MODE_PRIVATE
+        )
+
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+        val adapter = moshi.adapter(AccountModel::class.java)
+
+        binding.teste.apply {
+            val accountFromSharedPreferences = sharedPreferences.getString("Account", null)
+
+            if (accountFromSharedPreferences != null) {
+                val accountFromAdapter = adapter.fromJson(
+                    accountFromSharedPreferences
+                )
+
+                if (accountFromAdapter != null) {
+                    text =
+                        " ${accountFromAdapter.accountName}" +
+                                " ${accountFromAdapter.accountType}" +
+                                " ${accountFromAdapter.balance} " +
+                                " ${accountFromAdapter.responsible} "
+                }
+            }
+
+
         }
     }
 }
